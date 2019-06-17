f572f8cb94a069f2ae09406d144e4dabac9bacc6
erichdiniz <erichdiniz@hotmail.com>
2019-06-17 14:21:39 -0300

    Initial Commit

---
+++ RiR_POC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RiR_POC
+//
+//  Created by Erich Diniz on 17/06/19.
+//  Copyright © 2019 Erich Diniz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
