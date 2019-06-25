e9f7cd172b11074c2d7fd37cc93280a069f668f5
erichdiniz <erichdiniz@hotmail.com>
2019-06-25 12:12:44 -0300

    fonetics .images and .maps

--- RiR_POC/WatsonMock.swift
+++ RiR_POC/WatsonMock.swift
@@ -24,8 +24,8 @@ class WatsonMock: NSObject {
         return [
             WatsonMock(questionText: "Que horas começa o show do Foo Fighters?", responseText: "Fica ligado que faltam apenas 45 minutos pra começar a atração, gostaria de ser avisado quando começar?", responseType: ResponseType.schedule),
             WatsonMock(questionText: "O que é o Rock Street Asia?", responseText: "Rock Street Ásia será uma mostra de 5000 anos da história do mundo oriental. No espaço mais badalado do evento, a rua, com 200 metros de extensão fará um passeio pela arte, pelas tradições, pela cultura e promete fazer ainda uma apresentação lúdica da arquitetura destes povos.", responseType: ResponseType.normal),
-            WatsonMock(questionText: "O que vai tocar no palco sunset hoje?", responseText: "", responseType: ResponseType.images),
-            WatsonMock(questionText: "Qual o banheiro mais próximo?", responseText: "", responseType: ResponseType.maps)            
+            WatsonMock(questionText: "O que vai tocar no palco sunset hoje?", responseText: "No Palco samssét irá tocar:  Uaitisneick as 16 horas. Deseja agendar?", responseType: ResponseType.images),
+            WatsonMock(questionText: "Qual o banheiro mais próximo?", responseText: "O banheiro mais próximo vazio é: Róque Stríti 1.  ", responseType: ResponseType.maps)
         ]
     }
 
